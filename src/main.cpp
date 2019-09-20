7cb7465e0b8d4877e2b9ed1a396956e70afc8077
Bryson Gullett <brysongullett@gmail.com>
2019-09-20 18:49:16 -0400

    Added drive distance code that adds to the velocity of the motors gradually as time passes given a specific acceleration. It is constantly calculating how far it will end up going as it accelerates to a coast velocity and while it is driving at a coast velocity. Once it ends up calculating that the projected end point will be the target goal, then it decelerates to a stop, ideally at the goal. TODO: add a PID controller at the end of the function to definitively reach the target.

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,11 @@ double pController(double currentValue, double targetValue, double p) {
 
 // Get current distances
 double getCurrentDistanceLeft() {
-  return (leftF1.rotation(vex::deg) + leftB.rotation(vex::deg)) * 3.14159 * 2;
+  return ((leftF1.rotation(vex::deg) + leftB.rotation(vex::deg)) / 2) * (3.14159 / 180) * 2;
 }
 
 double getCurrentDistanceRight() {
-  return (rightF1.rotation(vex::deg) + rightB.rotation(vex::deg)) * 3.14159 * 2;
+  return ((rightF1.rotation(vex::deg) + rightB.rotation(vex::deg)) / 2) * (3.14159 / 180) * 2;
 }
 
 double getCurrentDistanceAvg() {
@@ -38,7 +38,18 @@ double getCurrentVelocityAvg() {
   return (getCurrentVelocityRight() + getCurrentVelocityLeft()) / 2;
 }
 
-// Drive straight specified distance in inches
+// Checks to see if robot should continue to accelerate based on how far robot is projected to move
+bool continueAccelerationPhase(double currentDistance, double targetDistance, double acceleration) {
+  
+  // Use kinematic equation to test distance robot will travel
+  double currentVelocity = getCurrentVelocityAvg();
+  double projectedDistance = ((currentVelocity * currentVelocity) / (2 * acceleration)) * 4 * 3.14159;
+
+  // Return whether or not the robot will travel the distance to the target end position upon deceleration to stop
+  return !(projectedDistance >= (targetDistance - currentDistance));
+}
+
+// Drive straight specified distance in inches; using p-controller
 void driveDistance(double distance) {
   double targetDistanceLeft = getCurrentDistanceLeft() + distance;
   double targetDistanceRight = getCurrentDistanceRight() + distance;
@@ -73,6 +84,79 @@ void driveDistance(double distance) {
   distanceToTargetAvg = targetDistanceAvg - getCurrentDistanceAvg();
 }
 
+// Drive inputted distance with inputted acceleration with inputted coast velocity
+// distance is in inches; acceleration is in RPM per minute; coast velocity is in RPM
+void driveDistanceAcceleration(double distance, double acceleration, double coastVelocity) {
+
+  vex::timer timer;
+  timer.clear();
+
+  int accelerationStage = 1;
+  double motorVelocity = 0;
+  double changeInMotorVelocity = 0;
+
+  // Calculate target average target distance for left and right sides of drivetrain
+  double targetLocationLeft = getCurrentDistanceLeft() + distance;
+  double targetLocationRight = getCurrentDistanceRight() + distance;
+  double targetLocationAvg = (targetLocationLeft + targetLocationRight) / 2;
+
+  double distanceToTargetAvg = targetLocationAvg - getCurrentDistanceAvg();
+
+  while(distanceToTargetAvg > 5.0 && distanceToTargetAvg < -5.0) {
+
+    // Find change in motor velocity based on specified acceleration
+    changeInMotorVelocity = acceleration * timer.time(vex::msec) * (0.00001667);
+
+    if(accelerationStage == 1) {
+
+      // Robot is accelerating; continue accelerating w/o passing goal
+      if(continueAccelerationPhase(getCurrentDistanceAvg(), targetLocationAvg, acceleration)) {
+        
+        // Accelerate
+        motorVelocity += changeInMotorVelocity;
+        
+        // Coast velocity has been reached; begin constant coast velocity stage
+        if(motorVelocity >= coastVelocity) {
+          motorVelocity = coastVelocity;
+          accelerationStage = 2;
+        }
+      } else {
+        // Robot will pass goal; begin deceleration phase
+        accelerationStage = 3;
+      }
+    } else if(accelerationStage == 2) {
+
+      // Robot is at coast velocity; see when it should start decelerating
+      if(continueAccelerationPhase(getCurrentDistanceAvg(), targetLocationAvg, acceleration)) {
+        accelerationStage = 3;
+      }
+    } else if(accelerationStage == 3) {
+      // Robot is decelerating; continue decelerating until stopped
+      if(motorVelocity >= 0) {
+        motorVelocity -= changeInMotorVelocity;
+      }
+    }
+
+    // Set velocities
+    leftF1.setVelocity(motorVelocity, vex::rpm);
+    leftF2.setVelocity(motorVelocity, vex::rpm);
+    leftB.setVelocity(motorVelocity, vex::rpm);
+    rightF1.setVelocity(motorVelocity, vex::rpm);
+    rightF2.setVelocity(motorVelocity, vex::rpm);
+    rightB.setVelocity(motorVelocity, vex::rpm);
+
+    // Spin motors
+    leftF1.spin(vex::fwd);
+    leftF2.spin(vex::fwd);
+    leftB.spin(vex::fwd);
+    rightF1.spin(vex::fwd);
+    rightF2.spin(vex::fwd);
+    rightB.spin(vex::fwd);
+
+    timer.clear();
+  }
+}
+
 int driver(void){
   double power, turn;
 
@@ -97,7 +181,7 @@ void usercontrol() {
 }
 
 void autonomous() {
-  driveDistance(20.0);
+  driveDistanceAcceleration(20.0, 10.0, 150.0);
 }
 
 int main() {
