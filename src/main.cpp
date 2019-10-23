b53ac62fdc8d4758b002227e63b35c1ae7ccd335
Bryson Gullett <brysongullett@gmail.com>
2019-10-23 19:31:32 -0400

    Edited code to drive a given distance with a specific acceleration. Does not slow down based on kinematics now.

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 
 using namespace vex;
 
-vex::competition Competition;
-
 // P (proportion) controller to calculate output based on error
 double pController(double currentValue, double targetValue, double p) {
   double error = targetValue - currentValue;
@@ -36,17 +34,6 @@ double getCurrentVelocityRight() {
 
 double getCurrentVelocityAvg() {
   return (getCurrentVelocityRight() + getCurrentVelocityLeft()) / 2;
-}
-
-// Checks to see if robot should continue to accelerate based on how far robot is projected to move
-bool continueAccelerationPhase(double currentDistance, double targetDistance, double acceleration) {
-  
-  // Use kinematic equation to test distance robot will travel
-  double currentVelocity = getCurrentVelocityAvg();
-  double projectedDistance = ((currentVelocity * currentVelocity) / (2 * acceleration)) * 4 * 3.14159;
-
-  // Return whether or not the robot will travel the distance to the target end position upon deceleration to stop
-  return !(projectedDistance >= (targetDistance - currentDistance));
 }
 
 // Drive straight specified distance in inches; using p-controller
@@ -87,74 +74,78 @@ void driveDistance(double distance) {
 // Drive inputted distance with inputted acceleration with inputted coast velocity
 // distance is in inches; acceleration is in RPM per minute; coast velocity is in RPM
 void driveDistanceAcceleration(double distance, double acceleration, double coastVelocity) {
-
+  double motorVelocity = 0;
   vex::timer timer;
   timer.clear();
 
-  int accelerationStage = 1;
-  double motorVelocity = 0;
-  double changeInMotorVelocity = 0;
-
-  // Calculate target average target distance for left and right sides of drivetrain
+  // Calculate target average distance for left and right sides of drivetrain
   double targetLocationLeft = getCurrentDistanceLeft() + distance;
   double targetLocationRight = getCurrentDistanceRight() + distance;
   double targetLocationAvg = (targetLocationLeft + targetLocationRight) / 2;
 
   double distanceToTargetAvg = targetLocationAvg - getCurrentDistanceAvg();
 
-  while(distanceToTargetAvg > 5.0 && distanceToTargetAvg < -5.0) {
+  double currentVelocityAvgAccumulator = 0.0;
+  int counter = 0;
+  double trueCurrentVelocityAvg = 0.0;
 
-    // Find change in motor velocity based on specified acceleration
-    changeInMotorVelocity = acceleration * timer.time(vex::msec) * (0.00001667);
+  double error = 0.0;
 
-    if(accelerationStage == 1) {
+  while(distanceToTargetAvg > 1.0 || distanceToTargetAvg < -1.0) {
 
-      // Robot is accelerating; continue accelerating w/o passing goal
-      if(continueAccelerationPhase(getCurrentDistanceAvg(), targetLocationAvg, acceleration)) {
-        
-        // Accelerate
-        motorVelocity += changeInMotorVelocity;
-        
-        // Coast velocity has been reached; begin constant coast velocity stage
-        if(motorVelocity >= coastVelocity) {
-          motorVelocity = coastVelocity;
-          accelerationStage = 2;
-        }
-      } else {
-        // Robot will pass goal; begin deceleration phase
-        accelerationStage = 3;
+    // Accumulator to measure velocity over 50ms period
+    currentVelocityAvgAccumulator += getCurrentVelocityAvg();
+    counter++;
+
+    if (timer.time() % 50 == 0) {
+
+      if(motorVelocity <= coastVelocity) {
+        motorVelocity += acceleration * timer.time() * 0.0000166667;
       }
-    } else if(accelerationStage == 2) {
 
-      // Robot is at coast velocity; see when it should start decelerating
-      if(continueAccelerationPhase(getCurrentDistanceAvg(), targetLocationAvg, acceleration)) {
-        accelerationStage = 3;
-      }
-    } else if(accelerationStage == 3) {
-      // Robot is decelerating; continue decelerating until stopped
-      if(motorVelocity >= 0) {
-        motorVelocity -= changeInMotorVelocity;
-      }
+      timer.clear();
+
+      distanceToTargetAvg = targetLocationAvg - getCurrentDistanceAvg();
+
+      trueCurrentVelocityAvg = currentVelocityAvgAccumulator / counter;
+
+      Brain.Screen.printAt(0, 35, "Calculated Velocity=%lf\n", motorVelocity);
+      Brain.Screen.printAt(0, 60, "Measured Velocity=%lf\n", trueCurrentVelocityAvg);
+
+      // Set velocities
+      leftF1.setVelocity(motorVelocity, vex::rpm);
+      leftF2.setVelocity(motorVelocity, vex::rpm);
+      leftB.setVelocity(motorVelocity, vex::rpm);
+      rightF1.setVelocity(motorVelocity, vex::rpm);
+      rightF2.setVelocity(motorVelocity, vex::rpm);
+      rightB.setVelocity(motorVelocity, vex::rpm);
+
+      // Spin motors
+      leftF1.spin(vex::fwd);
+      leftF2.spin(vex::fwd);
+      leftB.spin(vex::fwd);
+      rightF1.spin(vex::fwd);
+      rightF2.spin(vex::fwd);
+      rightB.spin(vex::fwd);
+
+      // Reset accumulator to find measured velocity over next 50ms
+      currentVelocityAvgAccumulator = 0;
+      counter = 0;
+
+      // Calculate percent error of actual current velocity vs assigned motorVelocity
+      error = (motorVelocity - trueCurrentVelocityAvg) / trueCurrentVelocityAvg;
+
+      //motorVelocity *= 1.0 + error;
     }
-
-    // Set velocities
-    leftF1.setVelocity(motorVelocity, vex::rpm);
-    leftF2.setVelocity(motorVelocity, vex::rpm);
-    leftB.setVelocity(motorVelocity, vex::rpm);
-    rightF1.setVelocity(motorVelocity, vex::rpm);
-    rightF2.setVelocity(motorVelocity, vex::rpm);
-    rightB.setVelocity(motorVelocity, vex::rpm);
-
-    // Spin motors
-    leftF1.spin(vex::fwd);
-    leftF2.spin(vex::fwd);
-    leftB.spin(vex::fwd);
-    rightF1.spin(vex::fwd);
-    rightF2.spin(vex::fwd);
-    rightB.spin(vex::fwd);
-
-    timer.clear();
   }
+
+  // Stop motors
+  leftF1.stop();
+  leftF2.stop();
+  leftB.stop();
+  rightF1.stop();
+  rightF2.stop();
+  rightB.stop();
 }
 
 int driver(void){
@@ -171,25 +162,16 @@ int driver(void){
     leftF1.spin(vex::fwd,power+turn, vex::rpm);
     leftF2.spin(vex::fwd,power+turn, vex::rpm);
 
-
     vex::task::sleep(10);
   }
 }
 
-void usercontrol() {
-  vex::task D = vex::task(driver);
-}
-
-void autonomous() {
-  driveDistanceAcceleration(20.0, 10.0, 150.0);
-}
-
 int main() {
+  //acclerateConstant();
+  driveDistanceAcceleration(25.0, 600.0, 75.0);
+  //vex::task D = vex::task(driver);
 
-  Competition.autonomous( autonomous );
-  Competition.drivercontrol( usercontrol );
-
-    while(true) {
-        vex::task::sleep(100);
-    }
+  while(true) {
+    vex::task::sleep(10);
+  }
 }
